99013c3b9e65b0cc88ed182aaccb0d0acd17c73c
fernandondm <109701072+fernandondm@users.noreply.github.com>
2023-09-14 16:27:55 -0300

    Update script.sh

--- k8s-deploy-app/script.sh
+++ k8s-deploy-app/script.sh
@@ -1,1 +1,5 @@
-#
+docker build -t hueland/projeto-backend:0.1 backend/.
+docker build -t huland/projeto-database:0.1 databse/.
+
+docker push hueland/projeto-backend:0.1
+docker push hueland/projeto-database:0.1
